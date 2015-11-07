c8004f74ed204a67e5aea8e56ba5afd747b31ea5
aravind <arvindsuresh2009@gmail.com>
2015-11-08 01:25:54 +0530

    added file upload, text to voice.

---
+++ public/js/file-upload.js
@@ -0,0 +1,32 @@
+var app = angular.module('myApp',['ngFileUpload']);
+
+app.controller('myCtrl', function($scope,Upload){
+	$scope.responsiveVoice = responsiveVoice;
+
+	$scope.uploadFile = function(file){
+		token = 0;
+		file.upload = Upload.upload({
+			url : '',
+			method : 'POST', 
+			headers : {
+				'Content-Type' : 'multipart/form-data',
+			}, 
+			fileFormDataName : 'uploadedFile',
+			fields : {
+				'__type__' : 'SEE_SHARP',
+				'__model_name__' : 'eng'
+			},
+			sendFieldAs : 'form',
+			file : file[0],
+		});
+
+		file.upload
+		.then(function(response){
+			$scope.text = response.output.join(' ');
+			console.log(text);
+			$scope.responsiveVoice.speak($scope.text, "UK English Male");
+		}, function(err){
+			console.log(err);
+		});
+	};
+});
